bc0225c5337d8eaa62f6c6cd194ccf53955bed5b
Spartasapp <spartateambuilder@mail.ru>
2019-03-24 19:46:36 +0300

    feat: Implement function which calculate how many zeros in the end of number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let n = number;
+  let factorial = [];
+  let k = 2;
+  while (k < base) {
+      while (base % k == 0) {
+          factorial.push(k);
+          base /= k;
+      }
+      k++;
+  }
+  if (base > 1) {
+      factorial.push(base);
+  }
+  factorial.sort((a, b) => a - b);
+
+  let zerosCount = [];
+  let count = 1;
+  for (let i = 0; i < factorial.length; i += count) {
+      let counter = 0;
+      for (let l = i; l < factorial.length; l++) {
+          if (factorial[i] === factorial[l]) {
+              counter++;
+          } else {
+              count = counter;
+              break;
+          }
+      }
+      let result = 0;
+      let l = factorial[i];
+      while (true) {
+          result += (n - n % l) / l;
+          l *= factorial[i];
+          if (l > n) {
+              break;
+          }
+      }
+      zerosCount.push((result - result % counter) / counter);
+  }
+
+  return Math.min.apply(Math, zerosCount);
 }
